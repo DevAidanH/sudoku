3c6545d9f1984e2f0c43e9ddbaeb41c687589950
DevAidan <aidanhumpidge333@gmail.com>
2024-12-06 17:30:58 +0000

    Still issues with the diffirent sized grids but almost there

--- validator.py
+++ validator.py
@@ -1,4 +1,4 @@
-# Test cases, one good and one bad
+# Test cases
 goodSudo = [
       [7,8,4, 1,5,9, 3,2,6],
       [5,3,9, 6,7,2, 8,4,1],
@@ -11,6 +11,22 @@ goodSudo = [
       [8,7,6, 3,9,4, 2,1,5],
       [2,4,3, 5,6,1, 9,7,8],
       [1,9,5, 2,8,7, 6,3,4]
+    ]
+
+smallGoodSudo = [
+      [1,4, 2,3],
+      [3,2, 4,1],
+    
+      [4,1, 3,2],
+      [2,3, 1,4]
+    ]
+
+smallBadSudo = [
+      [1,4, 4,3],
+      [3,2, 4,1],
+    
+      [4,1, 3,2],
+      [2,3, 1,4]
     ]
 
 badSudo = [
@@ -27,22 +43,29 @@ badSudo = [
       [1,2,3, 4,5,6, 7,8,9]
     ]
 
-def isValid(row, col, puzzle):
+missSizedSudo = [
+      [1,2,3,4,5],
+      [1,2,3,4],
+      [1,2,3,4],  
+      [1]
+    ]
+
+def isValid(row, col, puzzle, rangeRow, rangeCol):
     rowVal = puzzle [row]
     if rowVal.count(puzzle[row][col]) != 1:
         return False
     
-    colVal = [puzzle[i][col] for i in range(9)] #again modify for sizes
+    colVal = [puzzle[i][col] for i in range(rangeCol)]
     if colVal.count(puzzle[row][col]) != 1:
         return False
     
-    #Need to fix for smaller sized puzzles
-    rowStart = (row // 3) * 3
-    colStart = (col // 3) * 3
+    squareSize = rangeRow // rangeCol
+    rowStart = (row // squareSize) * squareSize
+    colStart = (col // squareSize) * squareSize
     
     squVal = []
-    for r in range(rowStart, rowStart + 3):
-        for c in range(colStart, colStart + 3):
+    for r in range(rowStart, rowStart + squareSize):
+        for c in range(colStart, colStart + squareSize):
             squVal.append(puzzle[r][c])
     if squVal.count(puzzle[row][col]) != 1:
         return False
@@ -50,11 +73,14 @@ def isValid(row, col, puzzle):
     return True
 
 def sudoValidation(puzzle):
-    #modify range for sizes
     count = 0
-    for r in range (9):
-        for c in range(9):
-            if isValid(r,c,puzzle):
+    rangeRow = len(puzzle)
+    rangeCol = len(puzzle[0]) # ISSUE This only works as it assumes all collums are the same size, This is not the case in the test cases so will need to find a way to check all 
+    if rangeCol != rangeRow:
+        return False
+    for r in range (rangeRow):
+        for c in range(rangeCol):
+            if isValid(r,c,puzzle, rangeRow, rangeCol):
                 count += 1
     if count == (len(puzzle)*len(puzzle)):
         return True
@@ -63,5 +89,9 @@ def sudoValidation(puzzle):
     
 
 
+#Test cases
 print(sudoValidation(goodSudo))
+print(sudoValidation(smallGoodSudo))
+print(sudoValidation(smallBadSudo))
 print(sudoValidation(badSudo))
+print(sudoValidation(missSizedSudo))
